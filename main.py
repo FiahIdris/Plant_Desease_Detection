7d9f8ab8dcf792668051b2c7e5115f038e7a1482
FiahIdris <nurfiahidris098@gmail.com>
2022-01-08 20:38:00 +0800

    add_model

--- main.py
+++ main.py
@@ -11,14 +11,20 @@ import os
 app = FastAPI()
 CROP_NAMES = ['Apple','Corn','Grape','Pepper','Potato','Strawberry','Tomato']
 CLASS_NAMES = ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy', 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 'Corn_(maize)___Common_rust', 'Corn_(maize)___Northern_Leaf_Blight', 'Corn_(maize)___healthy', 'Grape___Black_rot', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 'Grape___healthy', 'Pepper_bell___Bacterial_spot', 'Pepper_bell___healthy', 'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 'Strawberry___Leaf_scorch', 'Strawberry___healthy', 'Tomato___Bacterial_spot', 'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite', 'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus', 'Tomato___healthy']
+PLANT_NAMES = ['rice', 'maize', 'chickpea', 'kidneybeans', 'pigeonpeas','mothbeans', 'mungbean', 'blackgram', 'lentil', 'pomegranate','banana', 'mango', 'grapes', 'watermelon', 'muskmelon', 'apple','orange', 'papaya', 'coconut', 'cotton', 'jute', 'coffee']
 
-# class Crop(BaseModel):
-#     name: str
-#     price: float
-#     is_offer: Optional[bool] = None
+class Plant(BaseModel):
+    N: float
+    P: float
+    K: float
+    temp: float
+    humidity: float
+    pH: float
+    rainfall: float
     
 # Load models
 classifier = load_model('my_model.h5')
+recommender = load_model('recommender_system.h5')
 input_shape = classifier.layers[0].input_shape
 
 @app.get("/")
@@ -29,13 +35,23 @@ def main_page():
 def get_available_class():
     return {"crop_names":CROP_NAMES, "class_names": CLASS_NAMES,}
 
+@app.post("/predict-plant")
+async def predict_plant(plant:Plant):
+    prediction = recommender.predict([[plant.N, plant.P, plant.K, plant.temp, plant.humidity, plant.pH, plant.rainfall]])
+    index = prediction[0].argmax()
+    confidence = np.max(prediction[0])
+    return {
+        'prediction_class': PLANT_NAMES[index],
+        'confidence': np.round(float(confidence),4),
+    }
+
 
 # @app.put("/items/{item_id}")
 # def update_item(item_id: int, item:Item):
 #     return {"item_name":item.name, "item_id":item_id}
 
-@app.post("/predict")
-async def predict_new_image(file: UploadFile = File(...)):
+@app.post("/predict-leaf-disease")
+async def predict_leaf_disease(file: UploadFile = File(...)):
     # read image contain
     contents = await file.read()
     pil_image = Image.open(BytesIO(contents))
